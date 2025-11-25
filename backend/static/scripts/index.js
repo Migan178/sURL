d7584078b940b5bd1dfd3602f2a83eeddbed5645
Siwoo Jeon <me@migan.co.kr>
2025-11-25 18:08:01 +0900

    refactor: make style pretty

--- backend/static/scripts/index.js
+++ backend/static/scripts/index.js
@@ -1,8 +1,12 @@
-document.getElementById("find").addEventListener("submit", e => {
+document.getElementById("form").addEventListener("submit", e => {
+	if (e.submitter.id === "create") {
+		return;
+	}
+
 	e.preventDefault();
 
 	const url = document
-		.getElementById("find-url")
+		.getElementById("url")
 		.value.replace(window.location.href, "");
 	console.log(window.location);
 	window.location.href = `/info/${url}`;
