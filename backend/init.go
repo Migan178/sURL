2808778562cb365446043a7bd87243a230ef1566
Siwoo Jeon <me@miganbox.com>
2026-08-15 20:45:56 +0900

    fix: use go embed to make single binary
    I wanted to include static files in binary. But I didn't use go embed, so I cannot build a single binary. So I use go embed to fix it.

--- backend/init.go
+++ backend/init.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"embed"
+	"html/template"
+	"io/fs"
+	"net/http"
+
 	"github.com/Migan178/surl/routes"
 	"github.com/Migan178/surl/routes/api"
 	"github.com/Migan178/surl/routes/api/links"
@@ -9,12 +14,32 @@ import (
 
 var r *gin.Engine
 
+//go:embed static/*
+var staticFS embed.FS
+
 func init() {
 	r = gin.Default()
 
-	r.LoadHTMLGlob("static/templates/*.html")
-	r.Static("/styles", "./static/styles")
-	r.Static("/scripts", "./static/scripts")
+	tmpl, err := template.ParseFS(staticFS, "static/templates/*.html")
+	if err != nil {
+		panic(err)
+	}
+
+	r.SetHTMLTemplate(tmpl)
+
+	stylesFS, err := fs.Sub(staticFS, "static/styles")
+	if err != nil {
+		panic(err)
+	}
+
+	r.StaticFS("/styles", http.FS(stylesFS))
+
+	scriptsFS, err := fs.Sub(staticFS, "static/scripts")
+	if err != nil {
+		panic(err)
+	}
+
+	r.StaticFS("/scripts", http.FS(scriptsFS))
 
 	r.GET("/", routes.Home)
 	r.GET("/:urn", routes.Redirect)
